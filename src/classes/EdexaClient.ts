13d22c67fc1791d1264cae89c8b7c002212d2cb3
gautambnsl <g.bansal@edexa.team>
2023-09-06 11:54:25 +0530

    adding more comments in Edexa client file

--- src/classes/EdexaClient.ts
+++ src/classes/EdexaClient.ts
@@ -7,6 +7,12 @@ import { ERC1155 } from "./ERC1155Class";
 export class EdexaClient{
 
     //signers
+
+    /**
+   * Create a signer using a private key and provider URL.
+   * @param {string} pvtKey - The private key to create the signer.
+   * @returns {any} The signer object.
+   **/
     createWalletSigner(pvtKey : string){
         const wallet = new ethers.Wallet(pvtKey);
         const provider =  new ethers.providers.JsonRpcProvider("https://testnet.edexa.com/rpc");
@@ -14,6 +20,12 @@ export class EdexaClient{
         return signer;
     }
 
+
+    /**
+   * Create a signer using a provider object.
+   * @param {any} providerObject - The provider object to create the signer.
+   * @returns {any} The signer object.
+   **/
     createProviderSigner(providerObject: any){
     let provider = new ethers.providers.Web3Provider(providerObject);
     let signer = provider.getSigner()
@@ -22,14 +34,36 @@ export class EdexaClient{
 
 
     // ERC contracts
+
+    /**
+   * Get an instance of the ERC20 contract.
+   * @param {string} address - The address of the ERC20 contract.
+   * @param {string} rpc - The RPC URL (default is "https://testnet.edexa.com/rpc").
+   * @param {any} provider - The optional provider object.
+   * @returns {ERC20} An instance of the ERC20 contract.
+   **/
     getERC20Instance(address:string, rpc:string = "https://testnet.edexa.com/rpc" , provider?:any){
        return  new ERC20(address,rpc, provider)
     }
 
+    /**
+   * Get an instance of the ERC721 contract.
+   * @param {string} address - The address of the ERC721 contract.
+   * @param {string} rpc - The RPC URL (default is "https://testnet.edexa.com/rpc").
+   * @param {any} provider - The optional provider object.
+   * @returns {ERC721} An instance of the ERC721 contract.
+   **/
     getERC721Instance(address:string, rpc:string = "https://testnet.edexa.com/rpc" , provider?:any){
         return  new ERC721(address,rpc, provider)
     }
 
+     /**
+   * Get an instance of the ERC1155 contract.
+   * @param {string} address - The address of the ERC1155 contract.
+   * @param {string} rpc - The RPC URL (default is "https://testnet.edexa.com/rpc").
+   * @param {any} provider - The optional provider object.
+   * @returns {ERC1155} An instance of the ERC1155 contract.
+   **/
     getERC1155Instance(address:string, rpc:string = "https://testnet.edexa.com/rpc" , provider?:any){
         return  new ERC1155(address,rpc, provider)
     }
