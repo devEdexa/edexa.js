bd86564353358c2c05b511f5080c7a4cf0089401
gautam-edx <g.bansal@edexa.team>
2023-12-29 14:04:20 +0530

    chore: remove commented code from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -23,30 +23,3 @@ module.exports = {
   },
 }
 
-// const path = require('path');
-
-// module.exports = {
-//   mode: 'development', // or 'production'
-//   entry: './src/index.js',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env'],
-//           },
-//         },
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: ['.js','.ts'],
-//   },
-// };
