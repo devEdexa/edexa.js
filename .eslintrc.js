d9df3b1c799f027836018f8a0b520d1d8931284e
gautam-edx <g.bansal@edexa.team>
2023-12-29 13:56:24 +0530

    chore: modify .eslintrs.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,12 @@
 // .eslintrc.js
 module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   rules: {
     // Additional rules or overrides
